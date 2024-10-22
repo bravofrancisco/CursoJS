d288b55a836b5b801f73191e1c54f2e884e24385
Francisco Bravo <fcobravo.23@gmail.com>
2024-10-22 08:54:21 -0300

    Ejercicio de variables

---
+++ js/variables.js
@@ -0,0 +1,39 @@
+//#region
+// let producto = "playera";
+// let cantidad = 20;
+// let precio = 29.0;
+// let boolean = true;
+// const etiquetas = ["ropa", "verano", "invierno"];
+
+// console.log("producto", producto);
+// console.log("cantidad", cantidad);
+// console.log("precio", precio);
+// console.log("¿esta en oferta?", boolean);
+// console.log("etiquetas", etiquetas);
+//#endregion
+
+//#region
+// Define variables para almacenar tu nombre, edad, altura (en metros), si eres estudiante (booleano) y tus pasatiempos (arreglo de cadenas). Imprime toda la información en la consola.
+// let nombre = "lucas";
+// let edad = 31;
+// let altura = 1.60;
+// let IsStudents = true;
+// let pasatiempos = ["Playstation", "Jugar Futbol", "Gym"];
+
+// console.log('tu nombre', nombre);
+// console.log('tu edad',edad);
+// console.log('tu altura es', altura);
+// console.log('Eres estudiantes', IsStudents);
+// console.log('tus pasatiempos',pasatiempos);
+//#endregion
+
+//#region
+// Crea una variable que contenga tu año de nacimiento. Calcula tu edad actual (asumiendo que el año actual es 2024) y almacena el resultado en otra variable. Imprime tu edad.
+
+let AnoNacimiento = 1992;
+let AnoActual = 2024;
+let fechaActual = AnoActual - AnoNacimiento;
+
+console.log("tu edad actual es:", fechaActual);
+
+//#endregion
